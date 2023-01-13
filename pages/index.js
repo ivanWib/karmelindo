d1c0c2f266af7119525406756b2e819b5ef43973
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-01-13 13:48:22 +0700

    ganti nama

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 
-export default function Home() {
+export default function Index() {
   const [navbar, setNavbar] = useState(false);
 
   return (
