2aeb22c73446e30728ecb5e5284b979593e6c809
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-02-05 15:35:16 +0700

    fix gambar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Image from "next/image";
-// import Link from "next/link";
+import Link from "next/link";
 import Logo from "../public/Logo.png";
 import Gc from "../public/GC.png";
-import Sc from "../public/Sc.png";
+import Sc from "../public/SC.png"
 import Struktur from "../public/Struktur.png";
 
 
@@ -21,7 +21,7 @@ export default function Home() {
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <a href="/">
               <div className="flex gap-4 items-center cursor-pointer">
-                <Image src={Logo} width="100" height="100" alt="" />
+                <Image src="https://cdn.discordapp.com/attachments/887579866025832511/1051895626104848526/images.png" width="100" height="100" alt="" />
               </div>
             </a>
 
@@ -94,7 +94,7 @@ export default function Home() {
     {/* HERO */}
     <div className="grid md:flex justify-between bg-gray-300">
       <div className="flex justify-center md:m-28">
-        <Image src={Logo} alt="" className="w-36 mt-3 md:w-60 h-fit" />
+        <Image src="https://cdn.discordapp.com/attachments/887579866025832511/1051895626104848526/images.png" alt="" className="w-36 mt-3 md:w-60 h-fit" />
       </div>
 
       <div className="mx-5 md:w-1/2 text-justify md:mx-10">
@@ -123,16 +123,16 @@ export default function Home() {
 
 
     {/* JASA */}
-      {/* <div className="text-2xl mt-2 md:text-4xl grid justify-center font-ibm-plex-mono font-bold md:mt-5">Jasa</div>
+      <div className="text-2xl mt-2 md:text-4xl grid justify-center font-ibm-plex-mono font-bold md:mt-5">Jasa</div>
       <div className="flex justify-center gap-10 md:gap-20 m-5">
         <Link href="/gc">
-          <Image src={Gc} alt="" />
+          <Image src="https://cdn.discordapp.com/attachments/887579866025832511/1051895626104848526/images.png" alt="" />
         </Link>
         
         <Link href="/sc">
-          <Image src={Sc} alt="" />
+          <Image src="https://cdn.discordapp.com/attachments/887579866025832511/1051895626104848526/images.png" alt="" />
         </Link>
-      </div> */}
+      </div>
 
 
 
@@ -207,14 +207,14 @@ export default function Home() {
     <div className="bg-gray-300 grid justify-center gap-3">
       <div className="text-2xl md:text-4xl grid justify-center font-ibm-plex-mono font-bold mt-3">Struktur Organisasi</div>
       <div className="mb-3">
-        <Image src={Struktur} alt="" />
+        <Image src="https://cdn.discordapp.com/attachments/887579866025832511/1051895626104848526/images.png" alt="" />
       </div>
     </div>
 
     {/* FOOTER */}
     <div className="p-3 md:flex bg-red-600 text-white justify-between md:text-lg">
       <div className="md:m-5">
-        <Image src={Logo} width="100" height="100" alt="" />
+        <Image src="https://cdn.discordapp.com/attachments/887579866025832511/1051895626104848526/images.png" width="100" height="100" alt="" />
         <div className="mt-3 md:mt-5">
           Ruko Sutera Niaga IIE No.2, Raya Serpong KM 2, Tangerang 15325
         </div>
